392199246f2b5ef4b058e6b80a1056005255e7a9
Calebcai <37630280+Calebcai@users.noreply.github.com>
2021-10-11 15:31:09 +0800

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
     fromDisplayText: '蔡先森', // 收件箱展示的来件人名字
     fromDisplaySubText: '每日提醒', // 收件箱展示的次级标题
     user: '876057433@qq.com', // 发送者邮箱
-    pass: 'a123456789..', // 发送者邮箱MTP协议密码
+    pass: 'cpstsfidgitfbbbd', // 发送者邮箱MTP协议密码
     to: '691540026@qq.com', // 发送到谁，填邮箱
     weatherKey: '33369e365fe84eb68876f52a2ae51cca', // 和风天气key
     location: 'xxx',  // 和风天气-成都武侯区的id
