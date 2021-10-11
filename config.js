31abfb68c029f4208029fd2bf026c2eea3142147
Calebcai <37630280+Calebcai@users.noreply.github.com>
2021-10-11 18:45:34 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 // 配置信息
 module.exports = {
-    fromDisplayText: '蔡先森', // 收件箱展示的来件人名字
-    fromDisplaySubText: '每日提醒', // 收件箱展示的次级标题
+    fromDisplayText: '来自蔡先森的每日提醒⏰', // 收件箱展示的来件人名字
+    fromDisplaySubText: '早安❤️～今天又是元气满满的一天呢！', // 收件箱展示的次级标题
     user: '876057433@qq.com', // 发送者邮箱
     pass: 'cpstsfidgitfbbbd', // 发送者邮箱MTP协议密码
     to: '691540026@qq.com', // 发送到谁，填邮箱
     weatherKey: '7f28981709374cf592715dc8b707082c', // 和风天气key
     location: '101090213',  // 和风天气-成都武侯区的id
     type: '1,3,9', // 和风天气-生活指数type
-    tianXingKey: '8a88caae00f87c60a43645a10e6ed5dd', // 天行数据的key
+    tianXingKey: 'eb75297818f2413f24e1f1f76662bccd', // 天行数据的key
     startDay: '2021-6-29', // 在一起的日期
   }
   
