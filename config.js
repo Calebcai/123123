38ff7c81821bfef1281de0888089dc54b0461ad5
Calebcai <37630280+Calebcai@users.noreply.github.com>
2021-10-11 15:42:58 +0800

    Update config.js

--- config.js
+++ config.js
@@ -5,10 +5,10 @@ module.exports = {
     user: '876057433@qq.com', // 发送者邮箱
     pass: 'cpstsfidgitfbbbd', // 发送者邮箱MTP协议密码
     to: '691540026@qq.com', // 发送到谁，填邮箱
-    weatherKey: '33369e365fe84eb68876f52a2ae51cca', // 和风天气key
-    location: 'xxx',  // 和风天气-成都武侯区的id
+    weatherKey: '7f28981709374cf592715dc8b707082c', // 和风天气key
+    location: '101090213',  // 和风天气-成都武侯区的id
     type: '1,3,9', // 和风天气-生活指数type
-    tianXingKey: 'eb75297818f2413f24e1f1f76662bccd', // 天行数据的key
+    tianXingKey: '8a88caae00f87c60a43645a10e6ed5dd', // 天行数据的key
     startDay: '2021-6-29', // 在一起的日期
   }
   
